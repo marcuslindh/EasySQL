3064aa5df622a72ee173ba93041724d83cacd8c7
marcus lindh <marcusl1@hotmail.com>
2014-12-08 11:18:30 +0100

    added Tests

---
+++ EasySQL Tests/select_statement.vb
@@ -0,0 +1,33 @@
+﻿Imports System.Text
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+Imports EasySQL
+
+
+<TestClass()> Public Class select_statement
+
+    'Test Table (Name: TestTable)
+    '| ID | Name | LastName | Year |
+
+
+
+    <TestMethod()> Public Sub select_statement()
+        Dim sql As New SQLBuilder
+        Assert.AreEqual(sql.Select("ID, Name, LastName, Year").From("TestTable").GetSQL, "SELECT ID, Name, LastName, Year FROM TestTable")
+    End Sub
+
+    <TestMethod()> Public Sub select_statement_AllColumns()
+        Dim sql As New SQLBuilder
+        Assert.AreEqual(sql.Select("*").From("TestTable").GetSQL, "SELECT * FROM TestTable")
+    End Sub
+
+    <TestMethod()> Public Sub select_statement_Columns()
+        Dim sql As New SQLBuilder
+        Assert.AreEqual(sql.Select.Column("ID").Column("Name").Column("LastName").Column("Year").From("TestTable").GetSQL, "SELECT ID, Name, LastName, Year FROM TestTable")
+    End Sub
+
+    <TestMethod()> Public Sub select_statement_Columns_AllColumns()
+        Dim sql As New SQLBuilder
+        Assert.AreEqual(sql.Select.Column("*").From("TestTable").GetSQL, "SELECT * FROM TestTable")
+    End Sub
+
+End Class
